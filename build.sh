3cb43a420116e9b06574587005f119630208db52
Paola Quirós <66766320+paoqch@users.noreply.github.com>
2020-12-16 12:08:21 -0600

    Works

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Starting the build of the react application"
+npm run build
+
+echo "Packing the application into a docker image"
+docker build . -t star-wars-server
+
+echo "2/2 Steps completed"
